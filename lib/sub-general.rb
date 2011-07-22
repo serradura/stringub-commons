1e5bfe00f3637e03d79cef5ac9a13ee5a757017b
Rodrigo Serradura <rserradura@gmail.com>
2011-07-21 17:06:45 -0700

    String#unique_spaces replace a sequence of any kind of space per an unique space

--- lib/sub-general.rb
+++ lib/sub-general.rb
@@ -3,9 +3,20 @@ require "sub-general/version"
 module Sub
   module General
     WORD_PATTERN      = /\w[\w\'\-]*/
+    ANY_SPACE_PATTERN = /\s+/
+
+    UNIQUE_SPACE = " "
 
     def words
       self.scan(WORD_PATTERN)
+    end
+
+    def unique_spaces
+      self.gsub(ANY_SPACE_PATTERN, UNIQUE_SPACE)
+    end
+
+    def unique_spaces!
+      self.gsub!(ANY_SPACE_PATTERN, UNIQUE_SPACE)
     end
   end
 end
--- test/sub-general_test.rb
+++ test/sub-general_test.rb
@@ -2,6 +2,10 @@ require File.join(File.dirname(__FILE__), 'test_helper')
 
 class Sub::GeneralTest < Test::Unit::TestCase
 
+  def setup
+    @spaces_and_some_text = "    \n \r   \t\r\n foo \t bar    "
+  end
+
   def test_should_return_a_single_word_in_array
     assert_equal %w{Hello}, "Hello".words
   end
@@ -9,4 +13,38 @@ class Sub::GeneralTest < Test::Unit::TestCase
   def test_should_return_all_words_as_element_in_array
     assert_equal ["foo-foo", "bar's", "bar"], "foo-foo, bar's. bar!".words
   end
+
+  def test_should_replace_a_sequence_of_spaces_per_a_unique_space
+    string = @spaces_and_some_text.dup
+    assert_equal " foo bar ", string.unique_spaces
+  end
+
+  def test_should_replace_the_left_spaces_by_only_one
+    assert_equal '   \o/!'.unique_spaces, ' \o/!'
+  end
+
+  def test_should_replace_the_right_spaces_by_only_one
+    assert_equal '\o/!     '.unique_spaces, '\o/! '
+  end
+
+  def test_should_keep_the_original_content_after_calling_the_method
+    string = @spaces_and_some_text.dup
+    string.unique_spaces
+    assert_equal string, "    \n \r   \t\r\n foo \t bar    "
+  end
+
+  def test_should_return_the_same_content_if_any_space_replaced
+    assert_equal "Hello", "Hello".unique_spaces
+  end
+
+  def test_should_not_generate_a_new_object
+    string = @spaces_and_some_text.dup
+
+    assert_same string, string.unique_spaces!
+  end
+
+  def test_should_return_nil_if_nothing_changed
+    assert_nil "Hello".unique_spaces!
+  end
+
 end
