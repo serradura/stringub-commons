41a4026f398840bd4014e8d42149c65853252712
Rodrigo Serradura <rserradura@gmail.com>
2011-07-26 17:08:42 -0700

    Added source comments

--- README.markdown
+++ README.markdown
@@ -4,15 +4,38 @@
 
 <a href='https://rubygems.org/gems/stringub-commons'>https://rubygems.org/gems/stringub-commons</a>
 
-<a href="http://github.com/serradura/stringub_commons">http://github.com/serradura/stringub_commons</a>
+<a href="https://github.com/serradura/stringub-commons">https://github.com/serradura/stringub-commons</a>
 
 ## Description
 This library borned from the early versions of string_utility_belt gem, this gem adds new common purpose methods to String class. E.g: split a string in words, replace a sequence of spaces per a unique space.
 
 ## Install
 
-  gem install stringub_commons
+    gem install stringub_commons
 
 ## Examples
-  Examples can be founds in the tests.  
+### Method: words
+Split strings into an array of words.
+~~~
+  # Ex:
+  "Ruby on Rails".words
+  # >> ["Ruby", "on", "Rails"]
+
+  "Serradura's house".words
+  # >> ["Serradura's", "house"]
+~~~
+
+### Method: unique_spaces
+Removes a sequence of any kind of space characters per a unique whitespace.
+~~~
+  "       \n    abc    ".unique_spaces
+  # >> " abc "
+  
+  #Tip:
+   do you need remove trailing whitespaces. chain the String#strip method:
+  "       \n    abc    ".unique_spaces.strip
+  # >> "abc"
+~~~
+
+More examples can be founds in the tests.
 
--- lib/stringub-commons.rb
+++ lib/stringub-commons.rb
@@ -1,20 +1,46 @@
 require "stringub-commons/version"
 
+# Stringub::Commons provides new methods for strings.
 module Stringub
   module Commons
+    # Regexp pattern used to define words.
     WORD_PATTERN      = /\w[\w\'\-]*/
+    # Regexp pattern used to match any kind of space, ex: ' ', \n, \r, \t
     ANY_SPACE_PATTERN = /\s+/
 
     UNIQUE_SPACE = " "
 
+    # Split strings into an array of words.
+    #   # Ex:
+    #   "Ruby on Rails".words
+    #   # >> ["Ruby", "on", "Rails"]
+    #
+    #   "Serradura's house".words
+    #   # >> ["Serradura's", "house"]
+    #
+    #   "Module and sub-module".words
+    #   # >> ["Module", "and",  "aub-module"]
     def words
       self.scan(WORD_PATTERN)
     end
 
+    # Removes a sequence of any kind of space characters per a unique whitespace.
+    #   # Ex:
+    #   "       \n    abc    ".unique_spaces
+    #   # >> " abc "
+    #
+    #   "\o/   \n\r\t     ".unique_spaces
+    #   # >> "\o/ "
+    # ===Tip:
+    # If do you need remove trailing whitespaces. chain the String#strip method:
+    #   "       \n    abc    ".unique_spaces.strip
+    #   # >> "abc"
     def unique_spaces
       self.gsub(ANY_SPACE_PATTERN, UNIQUE_SPACE)
     end
 
+    # Removes a sequence of any kind of space characters per a unique whitespace.
+    # Returns nil if str was not altered.
     def unique_spaces!
       self.gsub!(ANY_SPACE_PATTERN, UNIQUE_SPACE)
     end
